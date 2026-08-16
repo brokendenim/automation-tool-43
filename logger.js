de3a9f8f2d000497279f623a7c426661a8a44510
Camille Laurent <traceytraceygentle061981@outlook.com>
2026-08-16 09:14:11 +0200

    add logging utility for network retries

--- logger.js
+++ logger.js
@@ -1,44 +1,33 @@
-const fs = require('fs');
-const path = require('path');
-
 class Logger {
-    constructor(logDir, maxSize, maxFiles) {
-        this.logDir = logDir;
-        this.maxSize = maxSize;
-        this.maxFiles = maxFiles;
-        this.logFile = path.join(logDir, 'app.log');
-        this.init();
+    constructor() {
+        this.levels = ['error', 'warn', 'info', 'debug'];
     }
 
-    init() {
-        if (!fs.existsSync(this.logDir)) {
-            fs.mkdirSync(this.logDir, { recursive: true });
-        }
-        if (!fs.existsSync(this.logFile)) {
-            fs.writeFileSync(this.logFile, '');
+    log(level, message) {
+        if (this.levels.includes(level)) {
+            const timestamp = new Date().toISOString();
+            console.log(`[${timestamp}] ${level.toUpperCase()}: ${message}`);
+        } else {
+            console.error('Invalid log level:', level);
         }
     }
 
-    log(message) {
-        const currentSize = fs.statSync(this.logFile).size;
-        if (currentSize >= this.maxSize) {
-            this.rotateLogs();
-        }
-        fs.appendFileSync(this.logFile, `${new Date().toISOString()}: ${message}\n`);
+    error(message) {
+        this.log('error', message);
     }
 
-    rotateLogs() {
-        for (let i = this.maxFiles - 1; i > 0; i--) {
-            const oldFile = path.join(this.logDir, `app.log.${i}`);
-            const newFile = path.join(this.logDir, `app.log.${i + 1}`);
-            if (fs.existsSync(oldFile)) {
-                fs.renameSync(oldFile, newFile);
-            }
-        }
-        const newLogFile = path.join(this.logDir, 'app.log.1');
-        fs.renameSync(this.logFile, newLogFile);
-        fs.writeFileSync(this.logFile, '');
+    warn(message) {
+        this.log('warn', message);
+    }
+
+    info(message) {
+        this.log('info', message);
+    }
+
+    debug(message) {
+        this.log('debug', message);
     }
 }
 
-module.exports = Logger;
+const logger = new Logger();
+export default logger;
